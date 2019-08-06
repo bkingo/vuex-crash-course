b3c820d7f8b00a6930ec8d32fa76526c2281d37d
Brandon King <brandonk@ocmi.systems>
2019-08-06 16:01:33 -0400

    More changes.

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
     <div id="app">
         <div class="container">
             <add-todo></add-todo>
+            <filter-todos></filter-todos>
             <todos></todos>
         </div>
     </div>
@@ -10,13 +11,15 @@
 <script>
 import Todos from "./components/Todos.vue";
 import AddTodo from "./components/AddTodo.vue";
+import FilterTodos from "./components/FilterTodos.vue";
 
 
 export default {
     name: "app",
     components: {
         Todos,
-        AddTodo
+        AddTodo,
+        FilterTodos
     }
 };
 </script>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,9 +1,19 @@
 <template>
     <div>
         <h3>Todos</h3>
+        <div class="legend">
+            <span>Double click to mark as complete</span>
+            <span>
+                <span class="incomplete-box"></span> = Incomplete
+            </span>
+            <span>
+                <span class="complete-box"></span> = Complete
+            </span>
+        </div>
         <div class="todos">
             <div v-for="todo in allTodos" :key="todo.id" class="todo">
                 {{ todo.title }}
+                <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
             </div>
         </div>
     </div>
@@ -15,29 +25,63 @@ import { mapGetters, mapActions } from "vuex";
 export default {
     name: "Todos",
     methods: {
-        ...mapActions(['fetchTodos']),
+        ...mapActions(["fetchTodos", "deleteTodo"])
     },
-    computed: mapGetters(['allTodos']),
+    computed: mapGetters(["allTodos"]),
     created() {
         this.fetchTodos();
     }
-}
+};
 </script>
 
 <style scoped>
-    .todos {
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        grid-gap: 1rem;
-    }
+.todos {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 1rem;
+}
 
-    .todo {
-        border: 1px solid #ccc;
-        background: #41b883;
-        padding: 1rem;
-        border-radius: 5px;
-        text-align: center;
-        position: relative;
-        cursor: pointer;
+.todo {
+    border: 1px solid #ccc;
+    background: #41b883;
+    padding: 1rem;
+    border-radius: 5px;
+    text-align: center;
+    position: relative;
+    cursor: pointer;
+}
+
+i {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+    color: #fff;
+    cursor: pointer;
+}
+
+.legend {
+    display: flex;
+    justify-content: space-around;
+    margin-bottom: 1rem;
+}
+
+.complete-box {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    background: #35495e;
+}
+
+.incomplete-box {
+    display: inline-box;
+    width: 10px;
+    height: 10px;
+    background: #41b883;
+}
+
+@media (max-width: 500px) {
+    .todos {
+        grid-template-columns: 1fr;
     }
+}
 </style>
